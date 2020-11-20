4c74354d7aec5f0b3dc7ef947787bfc3e048d456
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2020-11-19 19:22:15 -0500

    added knplabs/knp-paginator-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
