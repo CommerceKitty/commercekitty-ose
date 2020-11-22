a17013b414453b86fa0cd2e60be7b0022b56b737
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2020-11-21 21:53:15 -0500

    working on menus

---
+++ src/Menu/MenuBuilder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+class MenuBuilder
+{
+    /**
+     * @var FactoryInterface
+     */
+    private $factory;
+
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $dispatcher;
+
+    /**
+     * @param FactoryInterface         $factory
+     * @param EventDispatcherInterface $dispatcher
+     */
+    public function __construct(FactoryInterface $factory, EventDispatcherInterface $dispatcher)
+    {
+        $this->factory    = $factory;
+        $this->dispatcher = $dispatcher;
+    }
+
+    /**
+     * @param array $options
+     *
+     * @return ItemInterface
+     */
+    public function createGenericMenu(array $options): ItemInterface
+    {
+        if (empty($options['event_name'])) {
+            throw new \Exception('Must include "event_name" option.');
+        }
+
+        $menu = $this->factory->createItem('root');
+        $event = $this->dispatcher->dispatch(new GenericEvent($menu, $options), 'menu.'.$options['event_name']);
+
+        //$menu->addChild('home', ['route' => 'homepage']);
+        $menu->addChild('products', ['route' => 'product_index']);
+        //$menu['products']->addChild('view all', ['route' => 'homepage']);
+
+        return $menu;
+    }
+}
