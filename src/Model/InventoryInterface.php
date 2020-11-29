158c6189c2fec5f9bfc4fb0e381773546c1d0c9a
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2020-11-28 21:14:26 -0500

    work on inventory records

---
+++ src/Model/InventoryInterface.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace App\Model;
+
+/**
+ */
+interface InventoryInterface
+{
+    /**
+     * @return string
+     */
+    public function getId(): ?string;
+
+    /**
+     * Returns the warehouse this record is for
+     *
+     * @return WarehouseInterface
+     */
+    public function getWarehouse(): ?WarehouseInterface;
+
+    /**
+     * Returns the Product this record is for
+     *
+     * @return ProductInterface
+     */
+    public function getProduct(): ?ProductInterface;
+
+    /**
+     * Returns the number of Products in the Warehouse
+     *
+     * @return int
+     */
+    public function getQuantity(): ?int;
+}
